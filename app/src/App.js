aef008a0bec26b6c20b37ad5b29f457988195889
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-06-22 13:32:31 -0500

    Updated README.MD

--- app/src/App.js
+++ app/src/App.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import VideoPlayer from './components/VideoPlayer';
 import SearchBar from './components/SearchBar/SearchBar';
-import { RotatingLines } from "react-loader-spinner";
-
+import Loader from './components/Loader';
 
 function App() {
   const [url, setURL] =  useState("https://www.youtube.com/watch?v=JC90NBGC_bA");
@@ -23,53 +22,33 @@ function App() {
     };
   }, []);
 
-    const handleInputChange = (event) => {
-      const { value } = event.target;
-      setInputValue(value);
-    };
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
 
-    const handleSubmit = () => {
-      let counter;
-      setIsLoading(true);
-     
-        counter = setTimeout(() => {
-          setIsLoading(false);
-          setURL(inputValue);
-
-      }, 1000); // 2 second  
+  const handleSubmit = () => {
+    let counter;
+    setIsLoading(true);
+    
+      counter = setTimeout(() => {
+        setIsLoading(false);
+        setURL(inputValue);
+    }, 1000); // 1 second  
 
     setURL(inputValue);
-     return () => clearInterval(counter);
-     
+    return () => clearInterval(counter);
+  }
 
-
-    };
-
-const Loader = () => {
   return (
-    <RotatingLines
-      strokeColor="grey"
-      strokeWidth="5"
-      animationDuration="0.75"
-      width="200"
-      visible={true}
-
-      
-    />
-  )
-}
-  return (
-    <div className="App">
-      
+    <div className="App">      
       <header className="App-header">
         <h3>React Video Player Demo</h3>
         <p><i>Enter link below</i></p>
-        <SearchBar  value={inputValue}
-                 onChange={handleInputChange}
-                 onClick={()=>{handleSubmit()}}
+        <SearchBar  
+          value={inputValue} onChange={handleInputChange} onClick={()=>{handleSubmit()}}
         />
-        <div style={{marginBottom:5}}></div>
-       
+        <div style={{marginBottom:5}}></div>       
         <div style={{color:"black",borderWidth:10, padding: 2,width:"640px",
                     border:20,borderStyle:"solid", height:"360px" }}>
           {!isLoading? 
@@ -81,10 +60,8 @@ const Loader = () => {
               <Loader/>
             </div>
           }
-        </div>
-  
-      </header>
-      
+        </div>  
+      </header>      
     </div>
   );
 }
